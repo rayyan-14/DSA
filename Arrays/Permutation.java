7695c404b1c214cdd02c68cbccd545b115c20101
Rayyan Hanchanal <rayyanhanchanal@gmail.com>
2025-08-29 20:54:16 +0530

    Create Permutation.java

---
+++ Arrays/Permutation.java
@@ -0,0 +1,20 @@
+class Permutation {
+    
+    public static int[] buildArray(int[] nums) {
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+        
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int[] nums= new int[1000];
+        System.out.print("nums = ");
+        for(int i=0; i<1000; i++){
+            nums[i]=in.nextInt();
+        }
+        buildArray(nums);
+    }
+    }
