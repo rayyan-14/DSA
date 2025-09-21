89e0040ee82784fd101aaa847f5dc5ea59859416
Rayyan Hanchanal <rayyanhanchanal@gmail.com>
2025-09-21 22:59:05 +0530

    Create 744.java

---
+++ Searching/744.java
@@ -0,0 +1,20 @@
+class 744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int  start=0;
+        int end=letters.length;
+        if(target>=letters[letters.length-1]){
+            return letters[0];
+        }
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(letters[mid]<=target){
+                start=mid+1;
+            }
+            else end=mid-1;
+        }
+        return letters[start];
+
+        
+    }
+}
